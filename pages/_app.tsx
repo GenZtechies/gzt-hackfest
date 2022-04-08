e9539168aee67e1faf6dca364cbe9d1cf2f32f6d
Eniola Osabiya <eosabiya@gmail.com>
2022-04-08 01:23:47 +0100

    feat: favicon and SEO meta update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,26 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>gzt-hackFest2022</title>
                 <link rel="icon" href="/favicon.ico" />
+
+                {/* Primary Meta Tags */}
+                <title>GenZTechies HackFest 2022</title>
+                <meta name="title" content="GenZTechies HackFest 2022" />
+                <meta name="description" content="Africa's largest GenZ Hackathon and Festival" />
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://hackfest2022.genztechies.com/" />
+                <meta property="og:title" content="GenZTechies HackFest 2022" />
+                <meta property="og:description" content="Africa's largest GenZ Hackathon and Festival" />
+                <meta property="og:image" content="https://hackfest2022.genztechies.com/assets/site-preview.png" />
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://hackfest2022.genztechies.com/" />
+                <meta property="twitter:title" content="GenZTechies HackFest 2022" />
+                <meta property="twitter:description" content="Africa's largest GenZ Hackathon and Festival" />
+                <meta property="twitter:image" content="https://hackfest2022.genztechies.com/assets/site-preview.png" />
             </Head>
 
             <Component {...pageProps} />
