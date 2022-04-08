33f7d913f4ff4160fe2b939a2e28bbd4b032ed36
Eniola Osabiya <eosabiya@gmail.com>
2022-04-08 02:12:03 +0100

    chore: added theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
     mode: "jit",
     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: {
+                    purple: "#5D5CD6",
+                    yellow: "#FFB800",
+                    green: "#0DBA67"
+                }
+            }
+        }
     },
     plugins: []
 };
