ac3aba2cbaa606a8f27cd0657116aa23a97ed1c0
Eniola Osabiya <eosabiya@gmail.com>
2022-05-30 08:30:08 +0100

    chore: short links control

--- next.config.js
+++ next.config.js
@@ -7,7 +7,26 @@ const nextConfig = {
         HACKATHON_REGISTRATION_URL: "https://forms.gle/x5UNkUcDCDryLdsLA",
         CONFERENCE_REGISTRATION_URL: "https://eventprime.co/e/gen-z-hackfest-2022",
         BECOME_A_SPONSOR_URL: "https://airtable.com/shr3eOxjfYoHDqi8s"
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: "/speaking",
+                destination: "https://docs.google.com/forms/d/1jZETaw47OIapNRY2zDkVXmKkVzN8HjXWVaFdN3ZX81s/edit?usp=sharing",
+                permanent: false,
+            },
+            {
+                source: "/volunteer",
+                destination: "https://docs.google.com/forms/d/1SIxj-2eri8wqvRetUyT5a_yRdmpYQPj5zdsblWsPG_Q/edit?usp=sharing",
+                permanent: false,
+            },
+            {
+                source: "/showcase",
+                destination: "https://docs.google.com/forms/d/1zn8NLGRVAXA05_yeHwClTDnlFH_z2uzTr9M0cXl4wV4/edit?usp=sharing",
+                permanent: false,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
