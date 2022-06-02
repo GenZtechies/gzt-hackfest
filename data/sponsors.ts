a1ae9905f4591769f1d5d9166deac0b3046b69ac
Eniola Osabiya <eosabiya@gmail.com>
2022-06-02 14:59:21 +0100

    chore: sponsors update

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -4,6 +4,11 @@ const sponsors: any[] = [
     //     logo_image: "https://via.placeholder.com/600x600",
     //     reference_link: "" // Can be Twitter | Linkedin | Github | Website
     // },
+    {
+        name: "Filecoin",
+        logo_image: "https://docs.filecoin.io/logos/Filecoin-logo-blue+black.png",
+        reference_link: "https://filecoin.io/" // Can be Twitter | Linkedin | Github | Website
+    },
 ];
 
 export default sponsors;
--- pages/index.tsx
+++ pages/index.tsx
@@ -146,7 +146,7 @@ const Home: NextPage = () => {
 
                             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-10">
                                 {sponsors.map((sponsor: any, index: number) => (
-                                    <div key={index} className="bg-primary-yellow bg-opacity-50 p-2 rounded-lg shadow-lg hover:scale-105 transition duration-300">
+                                    <div key={index} className="bg-white bg-opacity-40 p-2 rounded-lg shadow-lg hover:scale-105 transition duration-300">
                                         <a href={sponsor.reference_link} target="_blank" rel="noopener noreferrer" title={sponsor.name}>
                                             <img src={sponsor.logo_image} alt={sponsor.name} className="w-full aspect-square object-contain object-center rounded-lg pb-2" />
                                         </a>
