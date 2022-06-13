66d3d44b1efd41104b6fc0d3b6dc76daf28a1b91
Eniola Osabiya <eosabiya@gmail.com>
2022-06-13 10:11:12 +0100

    chore: site meta update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,22 +18,22 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <link rel="icon" href="/favicon.ico" />
 
                 {/* Primary Meta Tags */}
-                <title>GenZTechies HackFest 2022</title>
-                <meta name="title" content="GenZTechies HackFest 2022" />
-                <meta name="description" content="Africa's largest GenZ Hackathon and Tech Festival 2022" />
+                <title>Gen Z Hackfest 2022</title>
+                <meta name="title" content="Gen Z Hackfest 2022" />
+                <meta name="description" content="Gen Z Hackfest is an annual hybrid event, 3-days hackathon and 1-day conference aimed at connecting Gen Z’s in tech on a large scale." />
 
                 {/* Open Graph / Facebook */}
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://hackfest2022.genztechies.com/" />
-                <meta property="og:title" content="GenZTechies HackFest 2022" />
-                <meta property="og:description" content="Africa's largest GenZ Hackathon and Tech Festival 2022" />
+                <meta property="og:title" content="Gen Z Hackfest 2022" />
+                <meta property="og:description" content="Gen Z Hackfest is an annual hybrid event, 3-days hackathon and 1-day conference aimed at connecting Gen Z’s in tech on a large scale." />
                 <meta property="og:image" content="https://hackfest2022.genztechies.com/assets/site-preview.png" />
 
                 {/* Twitter */}
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://hackfest2022.genztechies.com/" />
-                <meta property="twitter:title" content="GenZTechies HackFest 2022" />
-                <meta property="twitter:description" content="Africa's largest GenZ Hackathon and Tech Festival 2022" />
+                <meta property="twitter:title" content="Gen Z Hackfest 2022" />
+                <meta property="twitter:description" content="Gen Z Hackfest is an annual hybrid event, 3-days hackathon and 1-day conference aimed at connecting Gen Z’s in tech on a large scale." />
                 <meta property="twitter:image" content="https://hackfest2022.genztechies.com/assets/site-preview.png" />
             </Head>
 
