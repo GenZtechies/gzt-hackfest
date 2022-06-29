8e97c595e93296317912725afae272bcf1a4dc97
Eniola Osabiya <eosabiya@gmail.com>
2022-06-29 09:25:03 +0100

    chore: speakers update

--- data/speakers.ts
+++ data/speakers.ts
@@ -2,6 +2,7 @@ const speakers: any[] = [
     // {
     //     fullname: "Samantha Jane",
     //     role_title: "Software Developer",
+    //     type: "" // Can be speaker | judge
     //     company: "Microsoft",
     //     image: "https://via.placeholder.com/600x600",
     //     reference_link: "" // Can be Twitter | Linkedin | Github | Website
@@ -9,6 +10,7 @@ const speakers: any[] = [
     {
         fullname: "Sen. Tokunbo Abiru",
         role_title: "Lagos State Senator",
+        type: "speaker", // Can be speaker | judge
         company: "",
         image: "/assets/misc/people/tokunbo-abiru.jpeg",
         reference_link: "https://www.tokunboabiru.org/" // Can be Twitter | Linkedin | Github | Website
@@ -16,6 +18,7 @@ const speakers: any[] = [
     {
         fullname: "Jude Dike",
         role_title: "Co-founder & CEO",
+        type: "judge", // Can be speaker | judge
         company: "GetEquity",
         image: "/assets/misc/people/jude-dike.jpeg",
         reference_link: "https://twitter.com/bigbrutha_" // Can be Twitter | Linkedin | Github | Website
@@ -23,6 +26,7 @@ const speakers: any[] = [
     {
         fullname: "Csaba Zoltan Moldovan",
         role_title: "Venture Partner",
+        type: "judge", // Can be speaker | judge
         company: "Hiventures",
         image: "/assets/misc/people/csaba-moldovan.jpeg",
         reference_link: "https://www.linkedin.com/in/csabamoldovan/" // Can be Twitter | Linkedin | Github | Website
@@ -30,20 +34,39 @@ const speakers: any[] = [
     {
         fullname: "Favour Chukwuedo",
         role_title: "Tech Founder & CTO",
+        type: "speaker", // Can be speaker | judge
         company: "DigiLearns",
         image: "/assets/misc/people/favour-chukwuedo.jpeg",
         reference_link: "https://twitter.com/SenseiFavour" // Can be Twitter | Linkedin | Github | Website
     },
-    // {
-    //     fullname: "Deji Abisola",
-    //     role_title: "Hub Manager",
-    //     company: "SAIL",
-    //     image: "/assets/misc/people/favour-chukwuedo.jpeg",
-    //     reference_link: "https://twitter.com/SenseiFavour" // Can be Twitter | Linkedin | Github | Website
-    // },
+    {
+        fullname: "Deji Abisola",
+        role_title: "Hub Manager",
+        type: "judge", // Can be speaker | judge
+        company: "S.A.I.L",
+        image: "/assets/misc/people/deji-abisola.jpeg",
+        reference_link: "" // Can be Twitter | Linkedin | Github | Website
+    },
+    {
+        fullname: "Adebayo Oluwaseun",
+        role_title: "Developer Relations",
+        type: "judge", // Can be speaker | judge
+        company: "Spheron",
+        image: "/assets/misc/people/seun-adebayo.jpg",
+        reference_link: "https://www.linkedin.com/in/seunbayo/" // Can be Twitter | Linkedin | Github | Website
+    },
+    {
+        fullname: "Chukwuemeka Enoch Mbaebie",
+        role_title: "Orbit Community Ambassador, Devcon V Alumni, ETH Nigeria Convener",
+        type: "judge", // Can be speaker | judge
+        company: "",
+        image: "/assets/misc/people/chukwuemeka-enoch.jpg",
+        reference_link: "" // Can be Twitter | Linkedin | Github | Website
+    }
     // {
     //     fullname: "Seun Runsewe",
     //     role_title: "Senior Product Manager",
+    //     type: "speaker", // Can be speaker | judge
     //     company: "Chipper Cash",
     //     image: "/assets/misc/people/seun-runsewe.jpeg",
     //     reference_link: "https://www.linkedin.com/in/oluwaseun-runsewe/" // Can be Twitter | Linkedin | Github | Website
@@ -51,6 +74,7 @@ const speakers: any[] = [
     // {
     //     fullname: "Michael Okoh",
     //     role_title: "Co-Founder & CTO",
+    //     type: "speaker", // Can be speaker | judge
     //     company: "Thepeer",
     //     image: "https://naijabiography.com/wp-content/uploads/2022/06/Trojan.jpg",
     //     reference_link: "https://twitter.com/ichtrojan" // Can be Twitter | Linkedin | Github | Website
@@ -58,6 +82,7 @@ const speakers: any[] = [
     // {
     //     fullname: "Akintunde Sultan",
     //     role_title: "Co-Founder & CTO",
+    //     type: "speaker", // Can be speaker | judge
     //     company: "AltSchool Africa",
     //     image: "https://pbs.twimg.com/profile_images/1451624172266868739/lpi5wPb4_400x400.jpg",
     //     reference_link: "https://twitter.com/hackSultan" // Can be Twitter | Linkedin | Github | Website
@@ -65,6 +90,7 @@ const speakers: any[] = [
     // {
     //     fullname: "Kelvin Umechukwu",
     //     role_title: "Co-Founder & CEO",
+    //     type: "speaker", // Can be speaker | judge
     //     company: "Bumpa",
     //     image: "https://pbs.twimg.com/profile_images/1503092079727157257/ZYjFgsqD_400x400.jpg",
     //     reference_link: "https://twitter.com/tushgeek" // Can be Twitter | Linkedin | Github | Website
@@ -72,6 +98,7 @@ const speakers: any[] = [
     // {
     //     fullname: "Ada Nduka Oyom",
     //     role_title: "Founder",
+    //     type: "speaker", // Can be speaker | judge
     //     company: "SheCodeAfrica",
     //     image: "https://media-exp2.licdn.com/dms/image/C4E03AQELKicJ50jt0Q/profile-displayphoto-shrink_800_800/0/1643457844278?e=1661385600&v=beta&t=DIdkryfwHXg32qaYx3fLhL68nEkbCg7YKNhQJGHI3Q4",
     //     reference_link: "https://twitter.com/Kolokodess" // Can be Twitter | Linkedin | Github | Website
@@ -79,6 +106,7 @@ const speakers: any[] = [
     // {
     //     fullname: "Luqman Edu",
     //     role_title: "Partner",
+    //     type: "speaker", // Can be speaker | judge
     //     company: "Talencity",
     //     image: "https://media-exp2.licdn.com/dms/image/C5603AQHmXWh_kWifAQ/profile-displayphoto-shrink_800_800/0/1536957429587?e=1661385600&v=beta&t=UMtE3f-mFQjExTB93LpYa70VmvXuNsRLKiLtYn2NR88",
     //     reference_link: "https://www.linkedin.com/in/luqman-edu-07711451" // Can be Twitter | Linkedin | Github | Website
@@ -86,6 +114,7 @@ const speakers: any[] = [
     // {
     //     fullname: "Njoku Emmanuel",
     //     role_title: "Founder & CEO",
+    //     type: "speaker", // Can be speaker | judge
     //     company: "Lazerpay Finance",
     //     image: "https://i0.wp.com/cryptonewsherald.com/wp-content/uploads/2022/02/lazer.jpg?resize=886%2C570&ssl=1",
     //     reference_link: "https://www.linkedin.com/in/luqman-edu-07711451" // Can be Twitter | Linkedin | Github | Website
