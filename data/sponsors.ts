0bff93191f3768d2441851bafa1341dfda9d6418
Eniola Osabiya <eosabiya@gmail.com>
2022-07-02 07:41:35 +0100

    chore: sponsors added

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -15,6 +15,11 @@ const sponsors: any[] = [
         reference_link: "https://www.lazerpay.finance/" // Can be Twitter | Linkedin | Github | Website
     },
     {
+        name: "Zeddpay",
+        logo_image: "/assets/misc/companies/zeddpay.svg",
+        reference_link: "https://zeddpay.com/" // Can be Twitter | Linkedin | Github | Website
+    },
+    {
         name: "Spheron Protocol",
         logo_image: "/assets/misc/companies/spheron.svg",
         reference_link: "https://spheron.network/" // Can be Twitter | Linkedin | Github | Website
