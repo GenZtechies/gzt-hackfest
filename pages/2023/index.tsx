ccb5c34a47ec7408991c83b3678961d54b16a8ed
D'phenomenal <90291337+Dphenomenal101@users.noreply.github.com>
2023-04-26 15:22:26 +0100

    Added speaker app link :)

--- pages/2023/index.tsx
+++ pages/2023/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
                         <Link href="https://docsend.com/view/x3t5y66xybdyyxwq" className="btn btn-lg btn-wide bg-white hover:bg-white border-none normal-case text-primary-purple font-sora gap-4">
                             Become a Sponsor
                         </Link>
-                        <Link href="" className="btn btn-lg btn-wide bg-transparent hover:bg-transparent border-white hover:border-white normal-case text-white font-sora gap-4">
+                        <Link href="https://sessionize.com/genzhackfest-2023" className="btn btn-lg btn-wide bg-transparent hover:bg-transparent border-white hover:border-white normal-case text-white font-sora gap-4">
                             Speak at HackFest
                         </Link>
                     </div>
