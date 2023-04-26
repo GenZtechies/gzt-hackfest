9aa07805dea97113ca3db3821dd8d448195ec22e
D'phenomenal <90291337+Dphenomenal101@users.noreply.github.com>
2023-04-26 12:38:19 +0100

    Added the Twitter # to View past pics to drive retweets 😎

--- data/2023.ts
+++ data/2023.ts
@@ -1,3 +1,3 @@
 export const BUY_CONFERENCE_TICKET_URL = "https://eventprime.co/e/gen-z-hackfest-2023";
 export const HACKATHON_REGISTRATION_URL = "https://forms.gle/y5wRgVRFh1HabQrj7";
-export const HACKFEST_2022_PICTURES_URL = "https://photos.app.goo.gl/MnLqmyVyAn8ivCc19";
+export const HACKFEST_2022_PICTURES_URL = "https://twitter.com/hashtag/GenZHackfest2022?src=hashtag_click";
