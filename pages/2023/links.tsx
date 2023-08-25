540d3825235d637c3ef1bf2e87d38a7c2c68f965
Usman Hassan <usmanhassangu@gmail.com>
2023-08-25 19:54:40 +0100

    fjfkd

--- pages/2023/links.tsx
+++ pages/2023/links.tsx
@@ -1,5 +1,31 @@
+import { venue } from "@/data/2023";
+import Link from "next/link";
+const links = [
+    {
+        name: "Get DP",
+        link: "https://getdp.co/genzhackfest-2023"
+    },
+    {
+        name: "Locate Venue On Map",
+        link: venue
+    },
+    {}
+];
+
 const Links = () => {
-    return <div>Links</div>;
+    return (
+        <main className="flex items-center justify-center w-screen h-screen bg-white">
+            <ul className="space-y-2">
+                {links.map((link) => (
+                    <li>
+                        <Link href={link.link || "#"} className="font-semibold text-center text-black">
+                            {link.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 };
 
 export default Links;
