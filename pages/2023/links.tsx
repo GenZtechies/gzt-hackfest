666f7b07fd793c5ca04a678e251cbce83de204c2
Usman Hassan <usmanhassangu@gmail.com>
2023-08-25 21:21:52 +0100

    feat: update links page

--- pages/2023/links.tsx
+++ pages/2023/links.tsx
@@ -1,25 +1,25 @@
+import { Button } from "@/components/2023";
 import { venue } from "@/data/2023";
 import Link from "next/link";
 const links = [
     {
-        name: "Get DP",
+        name: "Get your personalized DP",
         link: "https://getdp.co/genzhackfest-2023"
     },
     {
         name: "Locate Venue On Map",
         link: venue
     },
-    {}
 ];
 
 const Links = () => {
     return (
         <main className="flex items-center justify-center w-screen h-screen bg-white">
-            <ul className="space-y-2">
+            <ul className="max-w-sm space-y-2">
                 {links.map((link) => (
-                    <li key={link.name} className="text-center text-primary-purple">
-                        <Link href={link.link || "#"} target="_blank" className="font-semibold">
-                            {link.name}
+                    <li key={link.name} className="w-full text-center text-primary-purple">
+                        <Link href={link.link || "#"} target="_blank" className="w-full font-semibold">
+                            <Button className="w-full">{link.name}</Button>
                         </Link>
                     </li>
                 ))}
